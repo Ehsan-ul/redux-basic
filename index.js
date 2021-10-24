1850b62ed9706f71085cb7430176e355e48e4868
Ehsanul Islam Sajjad <ehsanulislam82@gmail.com>
2021-10-24 20:58:55 +0600

    redux core concept: part 2

--- index.js
+++ index.js
@@ -7,9 +7,35 @@ const initState = {
 
 // reducer
 const rootReducer = (state = initState, action) => {
+  if (action.type === 'INCREMENT_NUM') {
+    return {
+      ...state,
+      num: state.num + 1,
+    };
+  }
+
+  if (action.type === 'ADD_NUM') {
+    return {
+      ...state,
+      num: state.num + action.value,
+    };
+  }
+
   return state;
 };
 
 // crate store
 const store = redux.createStore(rootReducer);
+// console.log(store.getState());
+
+// dispatching action
+store.dispatch({
+  type: 'INCREMENT_NUM', //always uppercase e likhte hoboe
+});
+
+store.dispatch({
+  type: 'ADD_NUM',
+  value: 34,
+});
+
 console.log(store.getState());
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,1 @@
-return state;
+...state,
